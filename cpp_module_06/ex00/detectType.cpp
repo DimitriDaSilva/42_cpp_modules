312535433ed52eb4eb2b508a138c1c76a6076ff9
Dimitri Da Silva <dimitri.gomes.da.silva@gmail.com>
2021-08-02 14:33:48 +0100

    Deleted couts

--- cpp_module_06/ex00/detectType.cpp
+++ cpp_module_06/ex00/detectType.cpp
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 19:28:17 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/08/02 10:11:33 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/08/02 14:32:07 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@ ABase* getType(std::string const& str_to_convert) {
 	ABase* type;
 
 	if (isFloat(str_to_convert)) {
-		std::cout << "is float!" << std::endl;
 		type = new Float(str_to_convert);
 	} else if (isDouble(str_to_convert)) {
-		std::cout << "is double!" << std::endl;
 		type = new Double(str_to_convert);
 	} else if (isChar(str_to_convert)) {
-		std::cout << "is char!" << std::endl;
 		type = new Char(str_to_convert);
 	} else if (isInt(str_to_convert)) {
-		std::cout << "is int!" << std::endl;
 		type = new Int(str_to_convert);
 	} else {
 		type = NULL;
